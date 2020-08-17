3d9b758c865352a70d548347eb8fa840ff38a76e
Mr.Wang <619511821@qq.com>
2020-08-17 15:51:53 +0800

    抽取数据库中的数据到init_data中，便于生产中初始化项目

---
+++ backend/app/app/db/export_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+from app.db.session import SessionLocal,engine
+db = SessionLocal()
+sql = """
+    SELECT TABLE_NAME FROM information_schema.`TABLES` WHERE TABLE_SCHEMA = 'DWDB' AND TABLE_NAME != 'alembic_version'
+"""
+rows = db.execute(sql).fetchall()
+for table in rows:
+    table_name = table[0]
+    sql  = f"""
+    SELECT * FROM DWDB.{table_name }
+    """
+
+    df = pd.read_sql(sql,con=engine)
+    file_path = os.getcwd() + f"/init_data/{table_name}.csv"
+    print(file_path)
+    df.to_csv(file_path,index=0)
