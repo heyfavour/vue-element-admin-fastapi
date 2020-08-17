139fad14d2bc59c3e962ac59ea137d85d7a14610
Mr.Wang <619511821@qq.com>
2020-08-17 15:50:55 +0800

    初始化将init_data中的数据导入数据库

--- backend/app/app/db/init_db.py
+++ backend/app/app/db/init_db.py
@@ -1,9 +1,11 @@
 from sqlalchemy.orm import Session
 
+import os
 from app import crud, schemas
 from app.core.config import settings
+from app.db.session import SessionLocal,engine
 from app.db import base  # noqa: F401
-
+import pandas as pd
 # make sure all SQL Alchemy models are imported (app.db.modules) before initializing DB
 # otherwise, SQL Alchemy might fail to initialize relationships properly
 # for more details: https://github.com/tiangolo/full-stack-fastapi-postgresql/issues/28
@@ -14,12 +16,20 @@ def init_db(db: Session) -> None:
     # But if you don't want to use migrations, create
     # the tables un-commenting the next line
     # Base.metadata.create_all(bind=engine)
+    path = os.getcwd()
+    if os.name in ('posix'):
+        init_data_path = os.getcwd() + "/db/init_data"
+    else:
+        init_data_path = os.getcwd() + "\\db\\init_data"
+    files = os.listdir(init_data_path)
+    print(files)
+    for file in files:
+        print(file)
+        if "csv" not in file:continue
+        try:
+            df  = pd.read_csv(init_data_path+"/"+file, sep=",")
+        except:
+            continue
+        df.to_sql(file.replace(".csv",""),engine,if_exists="append",index=False)
 
-    user = crud.user.get_by_username(db, email=settings.FIRST_SUPERUSER)
-    if not user:
-        user_in = schemas.UserCreate(
-            email=settings.FIRST_SUPERUSER_EMAIL,
-            password=settings.FIRST_SUPERUSER_PASSWORD,
-            is_superuser=True,
-        )
-        user = crud.user.create(db, obj_in=user_in)  # noqa: F841
+
