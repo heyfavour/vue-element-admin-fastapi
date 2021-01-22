15a177a7e5bc1f584ced27934b0a7aa177d7d572
Mr.Wang <619511821@qq.com>
2021-01-22 16:07:05 +0800

    add api global dependencies

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -1,14 +1,12 @@
+from fastapi import APIRouter, Depends
 
-from fastapi import APIRouter
-
+from app.api import deps
 from app.api.api_v1 import endpoints
 from app.api.api_v1 import system
 from app.api.api_v1 import report
 
-api_router = APIRouter()
-#各自模块的路由由各自模块负责
+api_router = APIRouter(dependencies=[Depends(Depends(deps.get_current_active_user))])
+# 各自模块的路由由各自模块负责
 api_router.include_router(endpoints.router)
 api_router.include_router(system.router)
 api_router.include_router(report.router)
-
-
