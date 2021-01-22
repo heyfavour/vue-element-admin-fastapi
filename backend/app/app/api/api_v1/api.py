e4f894629db8b0f2bd9bf1273843296f2e4b79e1
Mr.Wang <619511821@qq.com>
2021-01-22 16:20:36 +0800

    fix bug

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -5,7 +5,7 @@ from app.api.api_v1 import endpoints
 from app.api.api_v1 import system
 from app.api.api_v1 import report
 
-api_router = APIRouter(dependencies=[Depends(Depends(deps.get_current_active_user))])
+api_router = APIRouter(dependencies=[Depends(deps.get_current_active_user)])
 # 各自模块的路由由各自模块负责
 api_router.include_router(endpoints.router)
 api_router.include_router(system.router)
