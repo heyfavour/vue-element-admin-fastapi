19e6fe65b9eb54a0ce37a3e69e81087ff710afa7
Mr.Wang <619511821@qq.com>
2021-01-27 15:01:08 +0800

    角色权限表结构修改，新增角色顺序排序

--- backend/app/app/models/role.py
+++ backend/app/app/models/role.py
@@ -1,19 +1,20 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+from sqlalchemy.orm import relationship
 from app.db.base_class import Base
 
 
 class Role(Base):
     """权限组"""
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    key = Column(String(32), nullable=False)
-    name = Column(String(32), )
-    description = Column(String(128), )
+    name = Column(String(32), doc="权限组名称")
+    description = Column(String(128), doc="备注")
+    order = Column(Integer, doc="顺序")
+
+    role_menu = relationship("Role_Menu", backref="role")
 
 
 class Role_Menu(Base):
     """权限组-菜单-中间表"""
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     role_id = Column(Integer, ForeignKey("role.id"))
-    role_key = Column(String(32))
     menu_id = Column(Integer, ForeignKey("menu.id"))
--- backend/app/app/schemas/role.py
+++ backend/app/app/schemas/role.py
@@ -1,13 +1,14 @@
-from typing import Optional,Any
+from typing import Optional, Any
 
 from pydantic import BaseModel
 
 
 # Shared properties
 class RoleBase(BaseModel):
-    key: Optional[str] = None
-    name : Optional[str] = None
-    description : str
+    id: Optional[str] = None
+    name: Optional[str] = None
+    description: str
+    order: Optional[str] = None
     routes: Any = None
 
 
@@ -18,24 +19,4 @@ class RoleCreate(RoleBase):
 
 # Properties to receive on item update
 class RoleUpdate(RoleBase):
-    pass
-
-
-# Properties shared by models stored in DB
-class RoleInDBBase(RoleBase):
-    id: int
-    title: str
-    owner_id: int
-
-    class Config:
-        orm_mode = True
-
-
-# Properties to return to client
-class Role(RoleInDBBase):
-    pass
-
-
-# Properties properties stored in DB
-class RoleInDB(RoleInDBBase):
     pass
