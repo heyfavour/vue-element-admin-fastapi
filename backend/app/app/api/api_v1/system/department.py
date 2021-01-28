ec2586e93b0d8dcadb386d68f1e7cd309ed2deb5
Mr.Wang <619511821@qq.com>
2021-01-28 15:07:29 +0800

    部门管理 优化重构

--- backend/app/app/api/api_v1/system/department.py
+++ backend/app/app/api/api_v1/system/department.py
@@ -1,81 +1,56 @@
 from typing import Any
 
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session,joinedload_all
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
 from app import models, schemas
 from app.api import deps
-from app.extensions.utils import tree_children
-
+from app.extensions.utils import list_to_tree
 
 router = APIRouter()
 
+
 @router.get("/list", response_model=schemas.Response)
-def read_routes(db: Session = Depends(deps.get_db),
-                current_user: models.User = Depends(deps.get_current_active_user)
-                ) -> Any:
-    """
-    Retrieve Mock Data.
-    """
-    depts = db.query(models.Department
-                     ).options(joinedload_all("children", "children", "children","children")
-                               ).filter(models.Department.parent_id==None,models.Department.status==1).one()
-    depts = [tree_children(depts),]
-    return {
-        "code": 20000,
-        "data": depts,
-        "message":"",
-    }
+def get_departments(db: Session = Depends(deps.get_db), ) -> Any:
+    """部门管理-查询"""
+    departments = db.query(models.Department).all()
+    departments = list_to_tree([department.dict() for department in departments])
+    return {"code": 20000, "data": departments}
+
+
+@router.post("/", response_model=schemas.Response)
+def add_department(*, db: Session = Depends(deps.get_db), department: schemas.DepartmentCreate) -> Any:
+    """部门管理-新增"""
+    db.add(models.Department(**jsonable_encoder(department)))
+    return {"code": 20000, "data": "", "message": "新增部门成功"}
 
 
 @router.get("/{id}", response_model=schemas.Response)
-def read_routes(*,db: Session = Depends(deps.get_db),
-                id:int,
-                current_user: models.User = Depends(deps.get_current_active_user)
-                ) -> Any:
-    dept = db.query(models.Department).filter(models.Department.id == id).one()
+def get_department(*, db: Session = Depends(deps.get_db), id: int, ) -> Any:
+    """部门管理-更新前查询"""
+    department = db.query(models.Department).filter(models.Department.id == id).one()
+    return {"code": 20000, "data": department, }
 
-    return {
-        "code": 20000,
-        "data": dept,
-        "message":"",
-    }
-
-
-def tree_children_exclude(node, id):
-    if node.id == id:return {}
-    if node.children == []:
-        return node.dict()
-    else:
-        node_dict = node.dict()
-        node_dict["children"] = [tree_children_exclude(child,id) for child in node.children]
-    return node_dict
 
 @router.get("/list/exclude/{id}", response_model=schemas.Response)
-def read_routes(*,db: Session = Depends(deps.get_db),id:int,
-                current_user: models.User = Depends(deps.get_current_active_user)
-                ) -> Any:
-    """
-    Retrieve Mock Data.
-    """
-    depts = db.query(models.Department
-                     ).options(joinedload_all("children", "children", "children","children")
-                               ).filter(models.Department.parent_id==None,models.Department.status==1).one()
-    depts = [tree_children_exclude(depts,id)]
-    return {
-        "code": 20000,
-        "data": depts,
-        "message":"",
-    }
+def get_department_exclude_id(*, db: Session = Depends(deps.get_db), id: int, ) -> Any:
+    """返回一个排除了子节点的树结构"""
+    departments = db.query(models.Department).all()
+    departments = list_to_tree([department.dict() for department in departments], order="order", exclude=id)
+    return {"code": 20000, "data": departments, }
 
 
 @router.put("/", response_model=schemas.Response)
-def read_routes(*, db: Session = Depends(deps.get_db),data_in: schemas.DepartmentUpdate,current_user: models.User = Depends(deps.get_current_active_user)) -> Any:
-    """
-    Retrieve Mock Data.
-    """
-    db.query(models.Department).filter(models.Department.id == data_in.id).update(data_in)
-    return {
-        "code": 20000,
-        "data": "",
-        "message":"修改成功",
-    }
+def update_department(*, db: Session = Depends(deps.get_db), department: schemas.DepartmentUpdate) -> Any:
+    """部门管理-修改"""
+    db.query(models.Department).filter(models.Department.id == department.id).update(department)
+    return {"code": 20000, "message": "修改成功", }
+
+
+@router.delete("/{department_id}", response_model=schemas.Response)
+def delete_department_id(department_id: int, db: Session = Depends(deps.get_db), ) -> Any:
+    """部门管理-删除"""
+    users = db.query(models.User_Department).filter(models.User_Department.department_id == department_id).all()
+    if users != []: raise HTTPException(status_code=200, detail="该部门下存在员工，禁止删除")
+    db.query(models.Department).filter(models.Department.id == department_id).delete()
+    return {"code": 20000, "message": f"删除成功"}
