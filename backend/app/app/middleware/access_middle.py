3c244f8d0cf2bdfcbc783f89f2b3e5207c380f47
Mr.Wang <619511821@qq.com>
2021-01-28 15:09:38 +0800

    日志增加相应时间

--- backend/app/app/middleware/access_middle.py
+++ backend/app/app/middleware/access_middle.py
@@ -1,10 +1,15 @@
-from starlette.middleware.base import BaseHTTPMiddleware
+import datetime
 from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
 from app.extensions.logger import backend_logger
 
 
 class AccessMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
+        start_time = datetime.datetime.now()
         response = await call_next(request)
-        backend_logger.info(f"{request.client.host} {request.method} {request.url} {response.status_code}")
+        end_time = datetime.datetime.now()
+        backend_logger.info(
+            f"{request.client.host} {request.method} {request.url} {response.status_code} {end_time - start_time}"
+        )
         return response
