bcf0e584edcd1e3fdd7d12166def4fe6087a4f65
Mr.Wang <619511821@qq.com>
2021-01-28 17:30:35 +0800

    字典数据管理 优化重构

--- backend/app/app/schemas/system/dict.py
+++ backend/app/app/schemas/system/dict.py
@@ -5,9 +5,9 @@ from pydantic import BaseModel
 
 # Shared properties
 class DictTypeBase(BaseModel):
-    name :str
-    code :str
-    description:Optional[str] = None
+    name: str
+    code: str
+    description: Optional[str] = None
 
 
 # Properties to receive on item creation
@@ -20,19 +20,20 @@ class DictTypeUpdate(DictTypeBase):
     id: int
 
 
-
 class DictDataBase(BaseModel):
     """字典明细"""
-    label :str
-    order :int
-    remark :Optional[str] = None
-    type_id :str
-    css_class:Optional[str] = None
-    list_class:Optional[str] = None
-    is_default :Optional[bool] = None
+    label: str
+    order: int
+    remark: Optional[str] = None
+    type_id: str
+    css_class: Optional[str] = None
+    list_class: Optional[str] = None
+    is_default: Optional[bool] = None
+
 
 class DictDataCreate(DictDataBase):
     pass
 
+
 class DictDataUpdate(DictDataBase):
-    id : int
+    id: int
