78a350f18b7d123c5c6359465e7bfe9e04fb14b5
Mr.Wang <619511821@qq.com>
2021-01-29 15:53:14 +0800

    utils 修改树结构转换的逻辑方法

--- backend/app/app/extensions/utils.py
+++ backend/app/app/extensions/utils.py
@@ -10,7 +10,8 @@ from jose import jwt
 from app.core.config import settings
 
 
-def send_email(email_to: str, subject_template: str = "", html_template: str = "",environment: Dict[str, Any] = {}, ) -> None:
+def send_email(email_to: str, subject_template: str = "", html_template: str = "",
+               environment: Dict[str, Any] = {}, ) -> None:
     assert settings.EMAILS_ENABLED, "no provided configuration for email variables"
     message = emails.Message(
         subject=JinjaTemplate(subject_template),
@@ -18,9 +19,9 @@ def send_email(email_to: str, subject_template: str = "", html_template: str = "
         mail_from=(settings.EMAILS_FROM_NAME, settings.EMAILS_FROM_EMAIL),
     )
     smtp_options = {"host": settings.SMTP_HOST, "port": settings.SMTP_PORT}
-    if settings.SMTP_TLS:smtp_options["tls"] = True
-    if settings.SMTP_USER:smtp_options["user"] = settings.SMTP_USER
-    if settings.SMTP_PASSWORD:smtp_options["password"] = settings.SMTP_PASSWORD
+    if settings.SMTP_TLS: smtp_options["tls"] = True
+    if settings.SMTP_USER: smtp_options["user"] = settings.SMTP_USER
+    if settings.SMTP_PASSWORD: smtp_options["password"] = settings.SMTP_PASSWORD
     response = message.send(to=email_to, render=environment, smtp=smtp_options)
     logging.info(f"send email result: {response}")
     assert response.status_code == 250
@@ -29,17 +30,17 @@ def send_email(email_to: str, subject_template: str = "", html_template: str = "
 def send_test_email(email_to: str) -> None:
     project_name = settings.PROJECT_NAME
     subject = f"{project_name}-Test"
-    with open(Path(settings.EMAIL_TEMPLATES_DIR) / "test_email.html") as f:template_str = f.read()
+    with open(Path(settings.EMAIL_TEMPLATES_DIR) / "test_email.html") as f: template_str = f.read()
     send_email(
         email_to=email_to,
-        subject_template=subject,#主题
+        subject_template=subject,  # 主题
         html_template=template_str,
         environment={
             "project_name": settings.PROJECT_NAME,
             "email": email_to,
-            "msg":"这是一封测试邮件",
-            "emails_from_name":"Mr.Wang"
-        },#html template parameters
+            "msg": "这是一封测试邮件",
+            "emails_from_name": "Mr.Wang"
+        },  # html template parameters
     )
 
 
@@ -103,62 +104,48 @@ def verify_password_reset_token(token: str) -> Optional[str]:
         return None
 
 
-def deal_menus(menu):
-    """菜单Model生成dict Tree"""
-    meta = {
-        'title': menu.title,
-        "icon": menu.icon,
-        "noCache": menu.no_cache,
-        "affix": menu.affix,
-        "order": menu.order
-    }
-    if menu.children:
-        children = [deal_menus(menu) for menu in menu.children]
-        children.sort(key=lambda x: x["meta"]["order"])
-        menu_dict = menu.dict()
-        menu_dict['children'] = children
-    else:
-        menu_dict = menu.dict()
-    menu_dict['meta'] = meta
-    menu_dict['meta']["roles"] = [role.role_key for role in menu.roles]
-    return menu_dict
-
-
-def menus_to_list(menus):
-    """菜单dict生成list"""
-    if menus.get("children", []) == []: return [menus, ]
-    children = [menus_to_list(menu)[0] for menu in menus['children']]
-    menus.pop("children")
-    menus = [menus, ]
-    menus.extend(children)
-    return menus
-
-
-def tree_children(node):
-    if node.children == []: return node.dict()
-    node_dict = node.dict()
-    node_dict["children"] = [tree_children(child) for child in node.children]
-    return node_dict
-
-
-def list_to_tree(node_list, root_id=None):
+def list_to_tree(node_list, root_id=None, *, order="", exclude=None):
+    """
+        将list转成树结构 list = [{id:xx,parent_id:xx,},id:xx,parent_id:xx,},id:xx,parent_id:xx,}]
+        :param root_id  返回选择的id结点  order    排序字段   exclude  剔除某个节点
+    """
+    if not node_list: return []
+    # 排序
+    if order: node_list.sort(key=lambda k: k.get(order))
+    # node
     node_dict = {node["id"]: node if node.get("parent_id") else node.update({"parent_id": -1}) or node for node in
                  node_list}
+    # children
     for node in node_list:
+        if node["id"] == exclude: continue
         node_dict.setdefault(node["parent_id"], {}).setdefault("children", []).append(node)
-    return node_dict[root_id if root_id else -1]
+    if root_id:
+        return node_dict[root_id]
+    return node_dict.get(-1, {}).get("children", [])
 
 
-def get_list_id_by_tree(nodes):
-    ids = [nodes["id"], ]
-    if nodes.get("children"):
-        for node in nodes["children"]: ids = ids + get_list_id_by_tree(node)
+def dfs_tree_to_list(nodes):
+    """
+    将树结构转成list
+    :param nodes:
+    :return:
+    """
+    ids = []
+    for node in nodes:
+        ids.append(node["id"])
+        children = node.get("children", [])
+        if children != []: ids = ids + dfs_tree_to_list(children)
     return ids
 
 
 def round_float(float_num, num=2):
+    """
+    四舍五入
+    :param float_num:
+    :param num:
+    :return:
+    """
     import decimal
-    # 四舍五入
     context = decimal.getcontext()
     context.rounding = decimal.ROUND_HALF_UP
     return float(round(decimal.Decimal(str(float_num)), num))
