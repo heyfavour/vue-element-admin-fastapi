004a3c444974befc4a55194a8ac24bfc5102a144
Mr.Wang <619511821@qq.com>
2021-01-29 15:34:28 +0800

    用户管理 schema

--- backend/app/app/schemas/user.py
+++ backend/app/app/schemas/user.py
@@ -1,41 +1,38 @@
-from typing import Optional,List
+import datetime
 
-from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from pydantic import BaseModel
+
+from app.core.security import get_password_hash
+from app.core.config import settings
 
 
 # Shared properties
 class UserBase(BaseModel):
-    username : str
-    nickname : str
-    sex:str
-    identity_card :str
-    phone :Optional[str] = None
-    address :Optional[str] = None
-    work_start :Optional[str] = None
-    status :Optional[str] = None
-    hashed_password :Optional[str] = None
-    avatar :Optional[str] = None
-    introduction :Optional[str] = None
-
-    is_active :Optional[bool] = None
-    is_superuser :Optional[bool] = None
-
-
+    username: str
+    nickname: str
+    sex: str
+    identity_card: str
+    phone: Optional[str] = None
+    # address: Optional[str] = None
+    work_start: Optional[str] = datetime.datetime.today()
+    status: Optional[str] = None
+    hashed_password: str = get_password_hash(settings.INIT_PASSWORD)
+    # avatar: Optional[str] = None
+    introduction: Optional[str] = None
+    is_active: Optional[bool] = False
 
 
 # Properties to receive via API on creation
 class UserCreate(UserBase):
-    deptId:Optional[int] = None
-    postIds:List[int] = []
-    roleIds:List[int] = []
+    deptId: Optional[int] = None
+    postIds: List[int] = []
+    roleIds: List[int] = []
 
 
 # Properties to receive via API on update
 class UserUpdate(UserBase):
     id: Optional[int] = None
-    deptId:Optional[int] = None
-    postIds:List[int] = []
-    roleIds:List[int] = []
-
-
-
+    deptId: Optional[int] = None
+    postIds: List[int] = []
+    roleIds: List[int] = []
