0efa9ee4984604585be34eb430d455499e4042fc
Mr.Wang <619511821@qq.com>
2021-02-02 11:04:03 +0800

    Celery5.0启动方式

--- backend/app/worker-start.sh
+++ backend/app/worker-start.sh
@@ -1,3 +1,3 @@
 #指明队列名称
-ps -ux|grep 'celery worker'|grep -v grep|awk '{print $2}'|xargs kill -9
+ps -ux|grep 'celery'|grep -v grep|awk '{print $2}'|xargs kill -9
 nohup celery -A app.celery_app.worker.example worker -l info -Q example-queue -c 1  > celery.log &
