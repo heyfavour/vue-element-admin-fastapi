fd8d4952e93079d0aaebaa2949d061bebd251666
Mr.Wang <619511821@qq.com>
2021-02-02 10:23:01 +0800

    main启动脚本

--- backend/app/main-start.sh
+++ backend/app/main-start.sh
@@ -1,3 +1,2 @@
-netstat -anp |grep 8080|awk '{print $7}'|awk -F '/' '{print $1}'|xargs kill -9
-netstat -anp |grep 8080|awk '{print $7}'|awk -F '/' '{print $1}'|xargs kill -9
-nohup python app/main.py >> server.log & 
+ps -ux|grep python|grep main.py|awk '{print $2}'|awk -F '/' '{print $1}'|xargs kill -9
+nohup python app/main.py >> server.log &
