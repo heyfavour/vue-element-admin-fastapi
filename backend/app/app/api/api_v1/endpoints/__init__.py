5fa5719c3c614a97870d4a6493c26f311a81ef1b
Mr.Wang <619511821@qq.com>
2021-02-02 15:42:58 +0800

    transaction deleete

--- backend/app/app/api/api_v1/endpoints/__init__.py
+++ backend/app/app/api/api_v1/endpoints/__init__.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
-from app.api.api_v1.endpoints import login, utils, transaction, role
+from app.api.api_v1.endpoints import login, utils, role
 
 router = APIRouter()
 router.include_router(login.router, tags=["login"])
 router.include_router(utils.router, prefix="/utils", tags=["utils"])
-router.include_router(transaction.router, prefix="/transaction", tags=["transaction"])
 router.include_router(role.router, prefix="/role", tags=["role"])
