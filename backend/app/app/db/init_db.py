39cf9940be63ad310508554ac8de51e03d97766e
heyfavourite <619511821@qq.com>
2021-10-21 15:40:32 +0800

    修改初始化数据的AUTO_INCREMENT 开始值

--- backend/app/app/db/init_db.py
+++ backend/app/app/db/init_db.py
@@ -1,11 +1,12 @@
 import os, logging
 import pandas as pd
 import numpy as np
-from app.db.session import engine
+from app.db.session import engine,SessionLocal
 
 logger = logging.getLogger(__name__)
 
 def init_db() -> None:
+    session = SessionLocal()
     # Tables should be created with Alembic migrations
     init_data_path = os.path.join(os.path.dirname(__file__), "init_data")
     files = ['department.csv', 'menu.csv', 'role.csv', 'user.csv', 'dict_type.csv', 'dict_data.csv',
@@ -17,4 +18,9 @@ def init_db() -> None:
             df['component'] = df['component'].apply(lambda x: '' if pd.isnull(x) else x)
             df['name'] = df['name'].apply(lambda x: '' if pd.isnull(x) else x)
         logger.info(f"{file}  load successed")
-        df.to_sql(file.replace(".csv", ""), engine, if_exists="append", index=False)
+        table_name = file.replace(".csv", "")
+        df.to_sql(table_name, engine, if_exists="append", index=False)
+        sql = f"ALTER TABLE {table_name} AUTO_INCREMENT = {max(df['id']) + 1}"
+        session.execute(sql)
+    session.commit()
+    session.close()
