9a84e9f91f2869007095a5b73c99d9354055ab36
Mr.Wang <619511821@qq.com>
2022-04-06 18:16:32 +0800

    新增异常捕获日志 同时生成了两条出入记录

--- backend/app/app/middleware/access_middle.py
+++ backend/app/app/middleware/access_middle.py
@@ -6,10 +6,11 @@ from app.extensions.logger import backend_logger
 
 class AccessMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        start_time = datetime.datetime.now()
-        response = await call_next(request)
-        end_time = datetime.datetime.now()
-        backend_logger.info(
-            f"{request.client.host} {request.method} {request.url} {response.status_code} {end_time - start_time}"
-        )
+        backend_logger.info(f"{request.client.host} {request.method} {request.url}  [I]")
+        try:
+            response = await call_next(request)
+        except Exception as exc:
+            backend_logger.exception(exc)
+            raise exc
+        backend_logger.info(f"{request.client.host} {request.method} {request.url}  [0]")
         return response
