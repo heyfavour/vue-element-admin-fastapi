3705b6bff9adf67774181c8d6508908a71a3cb04
Mr.Wang <619511821@qq.com>
2022-04-06 18:17:07 +0800

    新增全链路追踪中间件

--- backend/app/app/middleware/__init__.py
+++ backend/app/app/middleware/__init__.py
@@ -2,10 +2,16 @@ from starlette.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
 from app.middleware.access_middle import AccessMiddleware
+from starlette_context.middleware import ContextMiddleware
+from starlette_context import plugins
 
 
 def register_middleware(app):
+    # midddleware fastapi是逆序注册的 所以最后注册RequestIdPlugin log reqeust_id 好让其他middleware使用
     app.add_middleware(AccessMiddleware)
+
+    app.add_middleware(ContextMiddleware, plugins=(plugins.RequestIdPlugin(),))
+
     if settings.BACKEND_CORS_ORIGINS:
         app.add_middleware(
             CORSMiddleware,
