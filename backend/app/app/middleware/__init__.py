1f4ca4aaf2bc7edfd3f3d71e7c42c0236748e457
wangzhenxiong <619511821@qq.com>
2025-03-01 11:08:07 +0800

    Update __init__.py
    request_id  starlette_context Context->RewContext

--- backend/app/app/middleware/__init__.py
+++ backend/app/app/middleware/__init__.py
@@ -2,7 +2,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
 from app.middleware.access_middle import AccessMiddleware
-from starlette_context.middleware import ContextMiddleware
+from starlette_context.middleware import RawContextMiddleware
 from starlette_context import plugins
 
 
@@ -10,7 +10,7 @@ def register_middleware(app):
     # midddleware fastapi是逆序注册的 所以最后注册RequestIdPlugin log reqeust_id 好让其他middleware使用
     app.add_middleware(AccessMiddleware)
 
-    app.add_middleware(ContextMiddleware, plugins=(plugins.RequestIdPlugin(),))
+    app.add_middleware(RawContextMiddleware, plugins=(plugins.RequestIdPlugin(),))
 
     if settings.BACKEND_CORS_ORIGINS:
         app.add_middleware(
